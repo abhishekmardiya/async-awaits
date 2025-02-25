e6b243db2a72ee0f37bc25f2eaf9c41f00c0fb27
abhishekmardiya <mardiyaabhishek@gmail.com>
2025-02-25 22:19:10 +0530

    Add new pages and navigation components; implement Tags, FindJobs, Community, Profile, Collection, and AskAQuestion pages with routing updates

---
+++ components/navigation/navbar/NavLinks.tsx
@@ -0,0 +1,74 @@
+"use client";
+
+import Image from "next/image";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { Fragment } from "react";
+
+import { SheetClose } from "@/components/ui/sheet";
+import { sidebarLinks } from "@/constants";
+import ROUTES from "@/constants/routes";
+import { cn } from "@/lib/utils";
+
+const NavLinks = ({ isMobileNav = false }: { isMobileNav: boolean }) => {
+  const pathname = usePathname();
+  // FIXME:dummy id
+  const userId = 1;
+
+  return (
+    <>
+      {sidebarLinks?.map((el) => {
+        const isActive =
+          (pathname?.includes(el?.route) && el?.route?.length > 1) ||
+          pathname === el?.route;
+
+        if (el?.route === ROUTES?.PROFILE) {
+          if (userId) {
+            el.route = `${el?.route}/${userId}`;
+          } else {
+            return null;
+          }
+        }
+
+        const linkComponent = (
+          <Link
+            href={el?.route}
+            key={el?.label}
+            className={cn(
+              isActive
+                ? "primary-gradient rounded-lg text-light-900"
+                : "text-dark300_light900",
+              "flex items-center justify-start gap-4 bg-transparent p-4"
+            )}
+          >
+            <Image
+              src={el?.imgURL}
+              alt={el?.label}
+              width={20}
+              height={20}
+              className={cn({ "invert-colors": !isActive })}
+            />
+            <p
+              className={cn(
+                isActive ? "base-bold" : "base-medium",
+                !isMobileNav && "max-lg:hidden"
+              )}
+            >
+              {el?.label}
+            </p>
+          </Link>
+        );
+
+        return isMobileNav ? (
+          <SheetClose asChild key={el?.route}>
+            {linkComponent}
+          </SheetClose>
+        ) : (
+          <Fragment key={el?.route}>{linkComponent}</Fragment>
+        );
+      })}
+    </>
+  );
+};
+
+export default NavLinks;
--- constants/routes.ts
+++ constants/routes.ts
@@ -2,6 +2,7 @@ const ROUTES = {
   HOME: "/",
   SIGN_IN: "/sign-in",
   SIGN_UP: "/sign-up",
+  PROFILE: "/profile",
 };
 
 export default ROUTES;
