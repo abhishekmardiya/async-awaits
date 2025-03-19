08a6a74153216e0d85b262efa36afd26eb18aecf
abhishekmardiya <mardiyaabhishek@gmail.com>
2025-03-19 22:04:25 +0530

    Add endpoint to retrieve user by email with validation and error handling

---
+++ app/api/users/email/route.ts
@@ -0,0 +1,29 @@
+import { NextRequest, NextResponse } from "next/server";
+
+import User from "@/database/user.model";
+import handleError from "@/lib/handlers/error";
+import { NotFoundError, ValidationError } from "@/lib/http-errors";
+import { UserSchema } from "@/lib/validations";
+
+// Get user by email
+export async function POST(req: NextRequest) {
+  const { email } = await req.json();
+
+  try {
+    const validatedData = UserSchema.partial().safeParse({ email });
+
+    if (!validatedData.success) {
+      throw new ValidationError(validatedData.error.flatten().fieldErrors);
+    }
+
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      throw new NotFoundError("User");
+    }
+
+    return NextResponse.json({ success: true, data: user }, { status: 200 });
+  } catch (error) {
+    return handleError(error, "api") as APIErrorResponse;
+  }
+}
