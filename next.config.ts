e32705d27540d1b5ae66026db6b0a7cfa2a124b3
abhishekmardiya <mardiyaabhishek@gmail.com>
2025-04-01 22:45:07 +0530

    Update project configuration and enhance security headers; bump version to 1.0.0

--- next.config.ts
+++ next.config.ts
@@ -6,6 +6,7 @@ const nextConfig: NextConfig = {
   },
   images: {
     remotePatterns: [
+      // FIXME:remove after testing
       {
         protocol: "https",
         hostname: "static.vecteezy.com",
@@ -15,6 +16,35 @@ const nextConfig: NextConfig = {
   },
   // to avoid errors when using pino with nextjs
   serverExternalPackages: ["pino", "pino-pretty"],
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "X-Frame-Options",
+            value: "SAMEORIGIN",
+          },
+          {
+            key: "X-Content-Type-Options",
+            value: "nosniff",
+          },
+          {
+            key: "Strict-Transport-Security",
+            value: "max-age=63072000; includeSubDomains; preload",
+          },
+          {
+            key: "Referrer-Policy",
+            value: "origin-when-cross-origin",
+          },
+          {
+            key: "X-DNS-Prefetch-Control",
+            value: "on",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
