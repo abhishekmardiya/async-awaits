18431eb7bdaa59f085933ea1062a5fe2e3784020
Abhishek Mardiya <mardiyaabhishek@gmail.com>
2026-01-01 22:24:34 +0530

    Add Tabs component for user profile page and integrate Stats display. Update ProfileLink export and enhance profile layout with new data sections.

--- components/user/ProfileLink.tsx
+++ components/user/ProfileLink.tsx
@@ -7,7 +7,7 @@ interface Props {
   title: string;
 }
 
-const ProfileLink = ({ imgUrl, href, title }: Props) => {
+export const ProfileLink = ({ imgUrl, href, title }: Props) => {
   return (
     <div className="flex-center gap-1">
       <Image src={imgUrl} alt={title} width={20} height={20} />
@@ -27,5 +27,3 @@ const ProfileLink = ({ imgUrl, href, title }: Props) => {
     </div>
   );
 };
-
-export default ProfileLink;
